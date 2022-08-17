8862a61f180e5e05efd9addbe9ed7c3681e539af
Jianne Gabriel <jiannegabriel@gmail.com>
2022-08-17 13:34:48 +0800

    Update configs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,51 +1,53 @@
-import '../styles/globals.css';
-import '@rainbow-me/rainbowkit/styles.css';
-import type { AppProps } from 'next/app';
-import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit';
-import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
+import '../styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css'
+import type { AppProps } from 'next/app'
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  connectorsForWallets,
+  wallet,
+} from '@rainbow-me/rainbowkit'
+import { chain, createClient, configureChains, WagmiConfig } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+import { jellieTestnet } from '../utils/Chain'
 
 const { chains, provider, webSocketProvider } = configureChains(
-  [
-    chain.mainnet,
-    chain.polygon,
-    chain.optimism,
-    chain.arbitrum,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true'
-      ? [chain.goerli, chain.kovan, chain.rinkeby, chain.ropsten]
-      : []),
-  ],
-  [
-    alchemyProvider({
-      // This is Alchemy's default API key.
-      // You can get your own at https://dashboard.alchemyapi.io
-      apiKey: '_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC',
-    }),
-    publicProvider(),
-  ]
-);
+  [jellieTestnet],
+  [publicProvider()]
+)
 
-const { connectors } = getDefaultWallets({
-  appName: 'RainbowKit App',
+const { wallets } = getDefaultWallets({
+  appName: 'RainbowKit Mint NFT Demo',
   chains,
-});
+})
+
+const demoAppInfo = {
+  appName: 'RainbowKit Mint NFT Demo',
+}
+
+const connectors = connectorsForWallets([
+  ...wallets,
+  {
+    groupName: 'Other',
+    wallets: [wallet.argent({ chains }), wallet.trust({ chains })],
+  },
+])
 
 const wagmiClient = createClient({
   autoConnect: true,
   connectors,
   provider,
   webSocketProvider,
-});
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
+      <RainbowKitProvider appInfo={demoAppInfo} chains={chains}>
         <Component {...pageProps} />
       </RainbowKitProvider>
     </WagmiConfig>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
